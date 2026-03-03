73101ad11309dc1d5c52c7c003b99adc754635a0
Evgeniy959 <77540303+Evgeniy959@users.noreply.github.com>
2026-03-03 23:35:13 +0700

    two commit

--- TestWtask/src/Main.java
+++ TestWtask/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        String s1 = "text";
+        String s2 = "Text".toLowerCase();
+        String s3 = "t"+"ext";
+
+        System.out.println(s2);
+        System.out.println(s1.equals(s2));
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
     }
 }
