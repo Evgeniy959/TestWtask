1f2a70748f20912533bd70b3691ca0c427527cd9
Evgeniy959 <77540303+Evgeniy959@users.noreply.github.com>
2026-03-04 02:21:22 +0700

    new commit

--- TestWtask/src/Main.java
+++ TestWtask/src/Main.java
@@ -9,5 +9,10 @@ public class Main {
         System.out.println(s1.equals(s2));
         System.out.println(s1 == s2);
         System.out.println(s1 == s3);
+
+        int[] arr = {2, 5, 6, 7, 8, 9};
+        for (int i = arr.length-1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
     }
 }
